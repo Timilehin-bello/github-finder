72803e4aaa7c0228ac09a90e2e90b2b31c16b7a7
Timilehin-bello <emmatimmybello@gmail.com>
2023-03-21 09:49:45 +0100

    Refactor usercard to a functional base component

--- src/components/UsersCard/UsersCard.jsx
+++ src/components/UsersCard/UsersCard.jsx
@@ -1,30 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 
-class UsersCard extends Component {
-  render() {
-    const { avatar_url, login, html_url } = this.props.user;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt="UserImage"
-          style={{ width: "60px" }}
-          className="round-img"
-        />
-        <h3>{login}</h3>
-        <div>
-          <a
-            href={html_url}
-            className="btn btn-dark btn-sm my-1"
-            target="_blank"
-            rel="noreferrer"
-          >
-            More
-          </a>
-        </div>
+const UsersCard = ({ user }) => {
+  const { avatar_url, login, html_url } = user;
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt="UserImage"
+        style={{ width: "60px" }}
+        className="round-img"
+      />
+      <h3>{login}</h3>
+      <div>
+        <a
+          href={html_url}
+          className="btn btn-dark btn-sm my-1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UsersCard;
